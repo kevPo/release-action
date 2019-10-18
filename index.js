2186e53c16689d1e892ecac4ce4263cbb3c60e3b
Kevin Poston <kposton@qualtrax.com>
2019-10-18 15:44:14 -0400

    fixed output

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ try {
   const releaseNumber = core.getInput('release-number');
   console.log(`Release Number: ${releaseNumber}!`);
   const time = (new Date()).toTimeString();
-  const message = `Successfully persisted release number and stuff at ${time} (not really but you know 😎)`;
+  const message = `Successfully persisted ${releaseNumber} and stuff at ${time} (not really but you know 😎)`;
   core.setOutput("message", message);
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  const payload = JSON.stringify(github.context.payload, undefined, 2);
   console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
